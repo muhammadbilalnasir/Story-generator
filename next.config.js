9f392295fc70f1b58a2d641f75236fdffd86cc06
muhammadbilalnasir <bilalkhan969@gmail.com>
2025-05-18 02:38:33 +0500

    changes to next

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 module.exports = {
+  output: "export", // Add this to enable static export in Next.js 14+
+
   webpack: (config, { isServer }) => {
     // Add transpilation for the problematic modules
     config.module.rules.push({
       test: /\.js$/,
       include: /node_modules\/undici/,
       use: {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env']
-        }
-      }
+          presets: ["@babel/preset-env"],
+        },
+      },
     });
     return config;
-  }
+  },
 };
